0cf9566e1152befff7cb63f64620fa4738fa5956
Kristaps Grinbergs <kristaps@qminderapp.com>
2017-02-18 01:26:32 +0200

    Initial Commit

---
+++ SmartBowl WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SmartBowl WatchKit Extension
+//
+//  Created by Kristaps Grinbergs on 18/02/2017.
+//  Copyright © 2017 fassko. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
